c928e6d8ab5ff80a63347af58a4420280d771329
Ranjith Rayaprolu <ranjith.rayaprolu@icloud.com>
2019-07-06 14:40:11 -0500

    Committed for some more examples.

--- Python/Python_Examples.py
+++ Python/Python_Examples.py
@@ -204,6 +204,18 @@ print("No. of Lower case Characters : ", d["lower"])
 
 up_low('To Test The Upper And Lower')
 
+#Write a Python function that takes a list and returns a new list with unique elements of the first list.
+#Sample List : [1,1,1,1,2,2,3,3,3,3,4,5]
+#Unique List : [1, 2, 3, 4, 5]
+
+def uniq_list(lst):
+    x = []
+    for a in lst:
+        if a not in x:
+            x.append(a)
+    return x
+
+uniq_list([1,1,1,1,2,2,3,3,3,3,4,5])
 
 
 
